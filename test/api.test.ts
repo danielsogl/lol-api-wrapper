487f9b76ef2682d39123216044ade4435cc11e05
Daniel <mytechde@outlook.com>
2018-05-09 16:52:43 +0200

    fix unit tetst

--- test/api.test.ts
+++ test/api.test.ts
@@ -1,7 +1,14 @@
 import request from 'supertest';
+import session from 'supertest-session';
 import app from '../src/app';
 
 describe('GET /', () => {
+  let testSession = undefined;
+
+  beforeEach(function() {
+    testSession = session(app);
+  });
+
   it('should get base route', () => {
     return request(app)
       .get('/')
